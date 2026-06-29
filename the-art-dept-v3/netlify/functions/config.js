d3b2ffd24123a87d63f90e189c3561e786984dfa
JamesBoulter <112915546+JamesBoulter@users.noreply.github.com>
2026-06-29 00:39:20 -0400

    Delete the-art-dept-v3 directory

--- the-art-dept-v3/netlify/functions/config.js
+++
@@ -1,9 +0,0 @@
-const { json } = require('./_supabase');
-
-exports.handler = async () => {
-  return json(200, {
-    supabaseUrl: process.env.SUPABASE_URL || '',
-    supabaseAnonKey: process.env.SUPABASE_ANON_KEY || '',
-    platformFeePercent: Number(process.env.PLATFORM_FEE_PERCENT || 5)
-  });
-};
